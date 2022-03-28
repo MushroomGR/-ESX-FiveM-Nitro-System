ac2bedb0d7e5210b2d0ff4b924f3ed19a2d65fbf
MushroomGR <40548229+MushroomGR@users.noreply.github.com>
2022-03-28 20:10:22 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,2 +1,19 @@
 Config = {}
-Config.mecanicjob  = false -- turn this to true if you want only mechanics to be able to install nitro to cars.
+Config.mechanicjob  = false -- turn this to true if you want only mechanics to be able to install nitro to cars inside ls customs and with the mechanic item you put bellow.
+Config.mechanicitem = 'blowpipe'
+Config.Zones = {
+				{
+				title = "Example Spot",                                                                                                                                                            
+				mapBlipId = 315,                                                            
+				mapBlipColor = 61,                                                           
+				spot = {x= 817.52, y = -2920.29, z= 5.21},
+				radius = 25,
+				},
+								{
+				title = "Example Spot 2",                                                                                                                                                            
+				mapBlipId = 315,                                                            
+				mapBlipColor = 61,                                                           
+				spot = {x= 220.99 , y = -851.99 , z= 29.51},
+				radius = 25,
+				},
+				}

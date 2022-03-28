d08f94745557ae531ffe535605b45e5a6e0b0bb0
MushroomGR <40548229+MushroomGR@users.noreply.github.com>
2022-03-28 20:27:08 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config = {}
-Config.mechanicjob  = false -- turn this to true if you want only mechanics to be able to install nitro to cars inside ls customs and with the mechanic item you put bellow.
+Config.mechanicjob  = false -- turn this to true if you want only mechanics to be able to install nitro to cars inside ls customs and with the mechanic item you put bellow.If you keen this false everyone will be able to install nitro to owned cars but only on spots you put on zones.
 Config.mechanicitem = 'blowpipe'
 Config.Zones = {
 				{
